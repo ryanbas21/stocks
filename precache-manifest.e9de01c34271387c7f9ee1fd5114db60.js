d27f4be212b11ff72aeb46acdcc3a8421c71d4ee
ryan <ryanbas21@gmail.com>
2018-11-15 22:04:32 -0600

    Updates

---
+++ precache-manifest.e9de01c34271387c7f9ee1fd5114db60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "411dab69da37cce9dcf9",
+    "url": "/stocks/static/css/main.1c7e3004.chunk.css"
+  },
+  {
+    "revision": "411dab69da37cce9dcf9",
+    "url": "/stocks/static/js/main.411dab69.chunk.js"
+  },
+  {
+    "revision": "1b5971d9203c043b3c27",
+    "url": "/stocks/static/js/1.1b5971d9.chunk.js"
+  },
+  {
+    "revision": "27b899632dbab2084591",
+    "url": "/stocks/static/js/runtime~main.27b89963.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stocks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4aea8a960a77195930128030b680041a",
+    "url": "/stocks/index.html"
+  }
+];
